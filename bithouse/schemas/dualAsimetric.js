207c06b09179c6936224d067448bf110a59d70a2
matiasMurat <matias.murat@bitlogic.io>
2022-11-11 13:35:19 -0300

    Dual Asimetric schema

---
+++ bithouse/schemas/dualAsimetric.js
@@ -0,0 +1,53 @@
+export default {
+  name: "DualAsimetric",
+  type: "document",
+  title: "Dual Asimetric",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Title",
+    },
+    {
+      name: "image",
+      type: "image",
+      title: "Image",
+    },
+    {
+      name: "imageSide",
+      type: "string",
+      title: "Image Side",
+      options: {
+        list: [
+          { title: "Left", value: "left" },
+          { title: "Right", value: "right" },
+        ],
+      },
+    },
+    {
+      name: "description",
+      type: "string",
+      title: "Description",
+    },
+    {
+      name: "content",
+      type: "string",
+      title: "Content",
+    },
+    {
+      name: "button",
+      type: "buttonObject",
+      title: "Button",
+    },
+    {
+      name: "colorLeft",
+      type: "color",
+      title: "Color Left",
+    },
+    {
+      name: "colorRight",
+      type: "color",
+      title: "Color Right",
+    },
+  ],
+};
